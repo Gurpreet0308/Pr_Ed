793e3cefade3a570ead021f94b88e38b2bbe511a
Gurpreet0308 <68944553+Gurpreet0308@users.noreply.github.com>
2020-08-05 16:25:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,30 @@
-const express = require("express");
-const path = require("path");
-const compression = require("compression");
-
-const app = express();
-
-app.engine("pug", require("pug").__express);
-
-app.set("view engine", "pug");
-app.set("views", path.join(__dirname, "views"));
-
-app.use(express.static(path.join(__dirname, "public")));
-
-app.use(compression());
-
-app.use("/", (req, res, next) => {
-  res.render("homepage");
-});
-
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-  console.log("server started at 3000");
-});
+    const express = require("express");
+    const path = require("path");
+    const compression = require("compression");
+     
+    const app = express();
+     
+    app.engine("pug", require("pug").__express);
+     
+    app.set("view engine", "pug");
+    app.set("views", path.join(__dirname, "views"));
+     
+    app.use(compression());
+     
+    const router = express.Router();
+     
+    router.all("/", (req, res) => {
+      console.log("hhhhhh");
+      res.render("intro");
+    });
+     
+    app.use("/", router);
+     
+    app.use(express.static(path.join(__dirname, "public")));
+     
+    const port = process.env.PORT || 8000;
+     
+    app.listen(port, () => {
+      console.log("server started at 3000");
+    });
+     
